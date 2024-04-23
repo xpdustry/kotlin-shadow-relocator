18cec4bacb2862c132850d536976754ad14ca9a0
phinner <62483793+phinner@users.noreply.github.com>
2024-04-23 04:02:33 +0200

    fix: Downgrade kotlin version to be the same as kotlin-dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.diffplug.spotless") version "6.25.0"
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "1.9.22"
     id("org.jetbrains.dokka") version "1.9.20"
     `java-gradle-plugin`
     id("com.gradle.plugin-publish") version "1.2.1"
